8239be13697408732a315a09da1bed864e6ddde4
panglimaa09-a11y <panglimaa09@gmail.com>
2026-09-03 22:50:04 +0700

    Add Google AdSense verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8124668365639299"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <VisitorTracker />
         {children}
